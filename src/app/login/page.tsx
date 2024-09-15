7ae0af7c340777c46419861258ebc1ab659416f2
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-15 12:47:40 +0530

    login and signup page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,41 @@
 "use client";
-import React from 'react'
+import React ,{useState}from 'react'
 import Link from "next/link"
 const LoginPage = () => {
+
+  const [userLogin , setUserLogin] = useState({email:'',password:''});
+
+  const signIn  = () =>{
+
+  }
   return (
     <div className='flex items-center flex-col mt-[25vh]'>
-      <div className='text-4xl'>
-        Login Page
-      </div>
-      <div className='m-2 rounded border-2 p-2'>
-        <Link href="/signup">SignUp</Link>
+      <div className='text-4xl my-5'>Login Page</div>
+      <div className='flex flex-col items-center border-2 p-3 rounded'>
+        <label htmlFor="email">Email</label>
+        <input
+           type="text"
+           id='email'
+           name="email"
+           placeholder='email'
+           value={userLogin.email}
+           onChange={(e)=>setUserLogin({...userLogin ,email:e.target.value})}
+           className='text-black px-2 py-1 my-1 border-none outline-none rounded-md'
+        />
+        <label htmlFor="password">password</label>
+        <input
+           type="password"
+           id='password'
+           name="password"
+           placeholder='password'
+           value={userLogin.password}
+           onChange={(e)=>setUserLogin({...userLogin ,password:e.target.value})}
+           className='text-black px-2 py-1 my-1 border-none outline-none rounded-md'
+        />
+        <div className='my-3'>
+          <button className='rounded border-2 px-1 mx-2 hover:bg-gray-600' onClick={signIn}>Login</button>
+          <Link href="/signup" className='rounded border-2 p-1 mx-2 hover:bg-gray-600'>SignUpPage</Link>
+        </div>
       </div>
     </div>
   )
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,53 @@
 "use client";
-import React from 'react'
+import React ,{ useState} from 'react'
 import Link from "next/link"
 const SignUp = () => {
+
+  const [ user , setUser] = useState({username:'',email:'',password:''});
+  console.log(user);
+
+  const signUp = () =>{
+
+  }
+
   return (
-    <div className='flex items-center flex-col mt-[25vh]'>
-      <div className='text-4xl'>SignUp Page</div>
-      <div className='m-2 rounded border-2 p-2'>
-        <Link href="/login">LoginPage</Link>
+    <div className='flex items-center flex-col mt-[20vh]'>
+      <div className='text-4xl my-5'>SignUp Page</div>
+      <div className='flex flex-col items-center border-2 p-3 rounded'>
+        <label htmlFor="username">Username</label>
+        <input
+           type="text"
+           id='username'
+           name="username"
+           placeholder='username'
+           value={user.username}
+           onChange={(e)=>setUser({...user ,username:e.target.value})}
+           className='text-black px-2 py-1 my-1 border-none outline-none rounded-md'
+        />
+        <label htmlFor="email">Email</label>
+        <input
+           type="text"
+           id='email'
+           name="email"
+           placeholder='email'
+           value={user.email}
+           onChange={(e)=>setUser({...user ,email:e.target.value})}
+           className='text-black px-2 py-1 my-1 border-none outline-none rounded-md'
+        />
+        <label htmlFor="password">password</label>
+        <input
+           type="password"
+           id='password'
+           name="password"
+           placeholder='password'
+           value={user.password}
+           onChange={(e)=>setUser({...user ,password:e.target.value})}
+           className='text-black px-2 py-1 my-1 border-none outline-none rounded-md'
+        />
+        <div className='my-3'>
+          <button className='rounded border-2 px-1 mx-2 hover:bg-gray-600' onClick={signUp}>Signup</button>
+          <Link href="/login" className='rounded border-2 p-1 mx-2 hover:bg-gray-600'>LoginPage</Link>
+        </div>
       </div>
     </div>
   )
